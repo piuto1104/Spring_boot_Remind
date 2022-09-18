37efd51d79a1f6c35968d93248b157bfb34fe6bf
piuto <piuto1104@nate.com>
2022-09-18 21:19:17 +0900

    Spring boot init

---
+++ src/main/java/io/wang/hello/Controller/HelloController.java
@@ -0,0 +1,13 @@
+package io.wang.hello.Controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+    @GetMapping("/hello")
+    public String hello(){
+        return "Hello Spring boot";
+    }
+}
